78b7c45cdeb1d601329d23d0d6c5e657e86cb039
jeremy bernaudon <jeremybernaudon13@gmail.com>
2020-05-05 18:29:49 +0200

    database tests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var PORT = 3000;
 
@@ -7,14 +9,43 @@ app.use(express.json({
     extended: false
 }));
 
+var con = mysql.createConnection({
+  host: "81.249.248.36",
+  port: "8457",
+  user: "ppd",
+  password: "ppdraspberrypi",
+  database: "tracking"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Successfully connected to the database !");
+});
+
+app.use(bodyParser.json())
+
 app.get('/', function(req, res){
-    res.render('index.ejs');
+  res.render('index.ejs');
 });
 
 app.post('/', function(req, res){
-    console.log(req.body);
-    //console.log(JSON.stringify(req.body, null, 4));
-    res.render('testjson.ejs', {data: req.body});
+  console.log(req.body);
+  var jsondata = req.body;
+  var trackingValues = [];
+
+  for(var i=0; i< jsondata.length; i++){
+    trackingValues.push([jsondata[i].position.latitude,jsondata[i].position.longitude])
+  }
+  con.query('INSERT INTO trackingData (latitude, longitude) VALUES ?', [values], function(err,result) {
+    if(err) {
+       res.send('Error in adding data to the database');
+    }
+   else {
+       res.send('Success in adding data to the database');
+    }
+  });
+
+  res.render('testjson.ejs', {data: req.body}); // move this line to the function requeting the database
 });
 
 //Start the server and make it listen for connections
